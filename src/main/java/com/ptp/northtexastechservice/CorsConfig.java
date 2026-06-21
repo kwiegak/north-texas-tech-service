853dca566887e28f6c3fb47233c43b0f9793cc29
kwiegak <rduke691@gmail.com>
2026-06-21 10:03:17 -0500

    automate-deploy-1

---
+++ src/main/java/com/ptp/northtexastechservice/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.ptp.northtexastechservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("https://planotechnologypartners.com");
+        config.addAllowedOrigin("https://www.planotechnologypartners.com");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
